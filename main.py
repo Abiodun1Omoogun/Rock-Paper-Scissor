7ca87eea16c92ee9fa25f6d89a3e70bbaabaa6f5
Emmanuel Omoogun <abiodunebun12@gmail.com>
2022-07-04 02:02:38 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,49 +1,45 @@
 # Rock, Paper, Scissors Game
 import random
-from xml.dom import InvalidAccessErr
 
 # moves for player  vs computer
 moves_options = ["R for rock","P for paper","S for scissor"]
-# random item from moves 
+print(moves_options)
 
 
 while True:
-   computer = random.choice(moves_options)
-   player = input("What moves_option did you choose: " " (or quit the game)").upper( )
-       
-   if player == "quit the game":
-      print("The game has ended.")
-      break
-
-
+   player = input("What moves_option did you choose? (or QUIT THE GAME)").upper( )
+   # print("random.choice(moves_options)")
+   computer = random.choice(["R for rock","P for paper","S for scissor"])  
+   
+   if player == "QUIT THE GAME":
+      print("THE GAME HAS ENDED.")
+      break 
+             
    elif player == "R":
-       if computer == "P":
-          print("You lose!", computer, "beats", player)
+       if computer == "S":
+          print("You win!", player, "beats", computer)  
        else:
-          print("You win!", player, "beats", computer)
+          computer == "P"
+          print("You lose!", computer, "beats", player) 
 
    elif player == "P":
-       if computer == "S":
-          print("You lose!", computer, "beats", player)
-       else:
-          print("You win!", player, "beats", computer)
-
-   elif player == "S":
        if computer == "R":
-          print("You lose!", computer, "beats", player)
+          print("You win!", player, "beats", computer)
        else:
-          print("You win!", player, "beats", computer) 
+          computer == "S"
+          print("You lose!", computer, "beats", player) 
    
-   elif player == computer:
-      print(" It a tie!")
-
-   else:
+   elif player == "S":
+       if computer == "P":
+          print("You win!", player, "beats", computer)
+       else:
+          computer == "R"
+          print("You lose!", computer, "beats", player) 
+   
+   else: 
+      player != moves_options
       # Invalid moves_options
       print("Invalid input, retry")
-
-
-# print(random.choice(moves))
-
-
-
-
+   
+   if player == computer:
+     print("It's a tie!", player, "Has the same moves_options with", computer)
